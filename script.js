b02c4e00a47d5971a9030296a7c46fa2d21f9275
Joosep Arrak <jooseparrak@gmail.com>
2021-12-08 21:10:04 +0200

    Added validation & alerts

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute() {
   // Set the values from elements
   p = document.getElementById("principal").value;
+
+  // if (p === 0) {
+  //   console.alert("Initial deposit cannot be zero.");
+  // } else if (p < 0) {
+  //   console.alert("Please enter a positive number.");
+  // } else return p;
+
   n = document.getElementById("years").value;
   r = document.getElementById("rate").value;
   result = document.getElementById("result");
@@ -11,19 +18,25 @@ function compute() {
   // Get the current year and add on top the value of n
   year = new Date().getFullYear() + parseInt(n);
 
-  // Put the result into the empty span under the Calculate button
-  result.innerHTML =
-    "If you deposit " +
-    p +
-    ", at the rate of " +
-    r +
-    "%<br />You will receive an amount of " +
-    interest +
-    ", in the year " +
-    year;
+  // Check the deposit amount and act accordingly
+  if (interest === 0) {
+    alert("Initial deposit cannot be zero");
+  } else if (interest < 0) {
+    alert("Please enter a positive number for the deposit.");
+  } else
+    result.innerHTML =
+      "If you deposit " +
+      p +
+      ", at the rate of " +
+      r +
+      "%<br />You will receive an amount of " +
+      interest +
+      ", in the year " +
+      year;
 
-  // Prevent browser window refresh
   event.preventDefault();
+
+  // // Put the result into the empty span under the Calculate button
 }
 
 function updateRate() {
